3d94f35b579b562bdec7fc82fcc742c5ad5959f6
Kiarash <kiarash@mit.edu>
2019-06-23 09:48:55 -0400

    lol

---
+++ app/ios/requestion/hello.h
@@ -0,0 +1,17 @@
+//
+//  hello.h
+//  requestion
+//
+//  Created by Kiarash on 6/20/19.
+//  Copyright © 2019 650 Industries, Inc. All rights reserved.
+//
+
+#import "RCTNetInfo.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface hello : RCTNetInfo
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ app/ios/requestion/hello.m
@@ -0,0 +1,13 @@
+//
+//  hello.m
+//  requestion
+//
+//  Created by Kiarash on 6/20/19.
+//  Copyright © 2019 650 Industries, Inc. All rights reserved.
+//
+
+#import "hello.h"
+
+@implementation hello
+
+@end
